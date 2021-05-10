a241693ffd4b353b1569e92d34dae6f90597b9d6
yashshukla0918 <69341705+yashshukla0918@users.noreply.github.com>
2021-05-11 02:15:42 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,26 +1,22 @@
 function hasUserMedia() { 
    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
-      || navigator.mediaDevices.getUserMedia || navigator.msGetUserMedia; 
+      || navigator.mozGetUserMedia || navigator.msGetUserMedia; 
    return !!navigator.getUserMedia; 
 }
  
 if (hasUserMedia()) { 
-   navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mediaDevices || navigator.msGetUserMedia;
+   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+      || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 		
    //get both video and audio streams from user's camera 
    navigator.getUserMedia({ video: true, audio: true }, function (stream) { 
       var video = document.querySelector('video'); 
 		
       //insert stream into the video tag 
-      	video.srcObject=stream ||  window.URL.createObjectURL(stream);
+      	video.srcObject=stream;
 	video.play();
    }, function (err) {}); 
 	
 }else {
    alert("Error. WebRTC is not supported!"); 
 }
-
-
-
-
