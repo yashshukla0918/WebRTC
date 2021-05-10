7463ad30a0597bb6ecd319ea995196e970e53648
yashshukla0918 <69341705+yashshukla0918@users.noreply.github.com>
2021-05-11 01:46:25 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 function hasUserMedia() { 
    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia; 
+      || navigator.mediaDevices.getUserMedia || navigator.msGetUserMedia; 
    return !!navigator.getUserMedia; 
 }
  
 if (hasUserMedia()) { 
    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+      || navigator.mediaDevices.getUserMedia || navigator.msGetUserMedia;
 		
    //get both video and audio streams from user's camera 
    navigator.getUserMedia({ video: true, audio: true }, function (stream) { 
