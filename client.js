767681867ff01f833bca256618fa6608ffc1dbf9
yashshukla0918 <69341705+yashshukla0918@users.noreply.github.com>
2021-05-11 02:10:48 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,21 +1,26 @@
 function hasUserMedia() { 
-   //check if the browser supports the WebRTC 
-   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || 
-      navigator.mozGetUserMedia); 
-} 
-
+   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
+      || navigator.mediaDevices.getUserMedia || navigator.msGetUserMedia; 
+   return !!navigator.getUserMedia; 
+}
+ 
 if (hasUserMedia()) { 
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mozGetUserMedia; 
+   navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+      || navigator.mediaDevices || navigator.msGetUserMedia;
 		
-   //enabling video and audio channels 
+   //get both video and audio streams from user's camera 
    navigator.getUserMedia({ video: true, audio: true }, function (stream) { 
       var video = document.querySelector('video'); 
 		
-      //inserting our stream to the video tag     
-      video.src =stream|| window.URL.createObjectURL(stream); 
-	  video.play();
+      //insert stream into the video tag 
+      	video.srcObject=stream ||  window.URL.createObjectURL(stream);
+	video.play();
    }, function (err) {}); 
-} else { 
-   alert("WebRTC is not supported"); 
+	
+}else {
+   alert("Error. WebRTC is not supported!"); 
 }
+
+
+
+
